cfe5289f153af132c8550dfcb2ba0161a4ba6a3c
Immanuel Sohn <info@immanuelsohn.de>
2023-11-20 02:13:09 +0100

    fixxed bottons and mobile alignment

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="min-h-screen flex flex-col justify-center items-center gap-5">
+    <div class="min-h-screen flex flex-col justify-center items-center gap-5 p-5">
         <h2 class="text-5xl md:text-6xl lg:text-7xl font-bold bg-clip-text 
             text-transparent bg-gradient-to-r from-zinc-600 to-zinc-950
             dark:from-white dark:to-gray-400">
                 Skills & Experiences
         </h2>
         <p>Over the past few years, I dived in technologies and frameworks of various kinds. These are the technologies I love and use the most:</p>
-        <div class="flex flex-col justify-center items-center gap-5 w-2/3 md:w-1/4">
+        <div class="flex flex-col justify-center items-center gap-5 w-4/5 md:w-1/4">
             <SkillProgress name="Java" icon="bxl:java" progress="90"/>
             <SkillProgress name="Python" icon="bxl:python" progress="70"/>
             <SkillProgress name="HTML" icon="bxl:html5" progress="85"/>
--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,12 +1,12 @@
 <template>
-<div class="min-h-screen flex flex-col justify-center items-center gap-5">
+<div class="min-h-screen flex flex-col justify-center items-center gap-5 p-5">
       <div class="flex flex-col justify-center items-center gap-3">
         <h1 class="text-5xl md:text-6xl lg:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-zinc-600 to-zinc-950 dark:from-white dark:to-gray-400">Welcome</h1>
         <p class="md:w-1/2 text-center">
           I'm a 20-years-old computer science student with a passion for all things related to computers. Let's connect and explore the possibilities of working together to bring your ideas to life. Get in touch and let's create something amazing!
         </p>
       </div>
-      <div class="flex gap-5 justify-center">
+      <div class="flex flex-col lg:flex-row gap-5 items-center justify-center">
         <LinkBotton href="https://instagram.com/immnl.shn" class="bg-red-500/90 hover:bg-red-600">
           <Icon name="uil:instagram" size="1.75rem"/>
           Instagram
